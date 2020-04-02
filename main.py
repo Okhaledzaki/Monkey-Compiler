6221cae3b9ac3e585cd1192970ba932d3483672c
omar659 <s-omarkhaled@zewailcity.edu.eg>
2020-04-02 23:30:01 +0200

    let's finish this book :-(

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import getpass
+import sys
 from lexer import *
 from tokenz import *
 name = getpass.getuser()
@@ -9,7 +10,7 @@ while True:
     line = input()
     l = Lexer(line)
     tokenz = l.nextToken().Type
-    while tokenz is not tokens.EOF:
+    while tokenz is not tokens.EOF:         # line by line
         print(tokenz,end=" ")
         tokenz = l.nextToken().Type
     print()
