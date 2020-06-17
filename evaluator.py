af3be458741507ac32662533d7e03b6a14edc817
omar-3 <s-omarkhaled@zewailcity.edu.eg>
2020-06-17 09:35:00 +0200

    evaluation infix expressions

--- evaluator.py
+++ evaluator.py
@@ -14,20 +14,12 @@ FALSE = _object.Boolean(Value = False)
 
 
 def Eval(node : ast.Node) -> _object.Object:
+
+    if isinstance(node, ast.ExpressionStatement):   # here node can't be ExpressionStatement
+        node = node.Expression                      # and InfixExpression at the same time
+    
     if isinstance(node, ast.Program):
         return evalStatements(node.Statements)
-    elif isinstance(node, ast.ExpressionStatement):
-        if isinstance(node.Expression, ast.IntegerLiteral):
-            return _object.Integer(Value = node.Expression.Value)
-        if isinstance(node.Expression, ast.Boolean):
-            return _object.Boolean(Value = node.Expression.Value)
-        if isinstance(node.Expression, ast.PrefixExpression):
-            right = Eval(node.Expression.Right)
-            return evalPrefixExpression(node.Expression.Operator, right)
-        if isinstance(node.Expression, ast.InfixExpression):
-            left = Eval(node.Expression.Left)
-            right = Eval(node.Expression.Right)
-            return evalInfixExpression(node.Expression.Operator, left, right) ######### ===> pick up from here
     elif isinstance(node, ast.IntegerLiteral):
         return _object.Integer(Value = node.Value)
     elif isinstance(node, ast.Boolean):
@@ -35,6 +27,10 @@ def Eval(node : ast.Node) -> _object.Object:
     elif isinstance(node, ast.PrefixExpression):
         right = Eval(node.Right)
         return evalPrefixExpression(node.Operator, right)
+    elif isinstance(node, ast.InfixExpression):
+        left = Eval(node.Left)
+        right = Eval(node.Right)
+        return evalInfixExpression(node.Operator, left, right)
     return None
 
 def evalStatements(stmts : List[ast.Statement]) -> _object.Object:
@@ -67,5 +63,23 @@ def evalBangOperatorExpression(right: _object.Object):
     else:
         return FALSE
 
+def evalInfixExpression(operator: str, left: _object.Object, right: _object.Object):
+    if isinstance(left, _object.Integer) and isinstance(right, _object.Integer):
+        return evalIntegerInfixExpression(operator, left, right)
+    else:
+        return NULL
 
 
+
+def evalIntegerInfixExpression(operator: str, left: _object.Object, right: _object.Object):
+    leftVal = left.Value
+    rightVal = right.Value
+    if operator == "+":
+        return _object.Integer(Value = leftVal + rightVal)
+    if operator == "-":
+        return _object.Integer(Value = leftVal - rightVal)
+    if operator == "*":
+        return _object.Integer(Value = leftVal * rightVal)
+    if operator == "/":
+        return _object.Integer(Value = leftVal / rightVal)
+    return NULL
--- test_evaluator.py
+++ test_evaluator.py
@@ -9,7 +9,12 @@ def TestEvalIntegerExpression():
         def __init__(self, input, expected):
             self.input = input
             self.expected = expected
-    tests = [struct("5", 5), struct("10", 10)]
+    tests = [struct("5", 5), struct("10", 10),
+             struct("5 + 5 + 5 + 5 - 10", 10),
+             struct("2 * 2 * 2 * 2 * 2", 32),
+             struct("- 50 + 100 - 50", 0),
+             struct("20 + 2 * -10", 0),
+             struct("3 * ( 3 * 3 ) + 10", 37)]
     for test in tests:
         l = lexer.Lexer(test.input)
         p = parser.Parser(l)
@@ -50,20 +55,6 @@ def TestBangOperator():
         testBooleanObject(obj, test.expected)
     print("TestBangOperator is done yayyy")
 
-def TestEvalIntegerExpression():
-    class struct:
-        def __init__(self, input, expected):
-            self.input = input
-            self.expected = expected
-    tests = [struct("5", 5), struct("10", 10), struct("-5", -5), struct("-10", -10)]
-    for test in tests:
-        l = lexer.Lexer(test.input)
-        p = parser.Parser(l)
-        program = p.ParseProgram()
-        obj = evaluator.Eval(program)
-        testIntegerObject(obj, test.expected)
-    print("Test Integers is done yayy")
-
 def testBooleanObject(obj: _object.Object, expected: bool):
     assert isinstance(obj, _object.Boolean)
     assert obj.Value == expected
@@ -75,7 +66,6 @@ def testIntegerObject(obj: _object.Object, expected: int):
 
 TestEvalIntegerExpression()
 TestEvalBooleanExpression()
-TestEvalIntegerExpression()
 TestBangOperator()
 
 
